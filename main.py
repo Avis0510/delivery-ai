27768090c3dd1f24f247b7d4da63530162fa7840
Avis <avisz0510@gmail.com>
2026-06-25 16:02:51 +0200

    step 1 shop system

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI
 import sqlite3
 import uuid
-import joblib
-import numpy as np
 import os
-
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model.pkl")
-model = joblib.load(MODEL_PATH)
 
 app = FastAPI()
 
-# DB (SAUBER)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(_file_))
 DB_PATH = os.path.join(BASE_DIR, "saas.db")
 
 conn = sqlite3.connect(DB_PATH, check_same_thread=False)
@@ -25,32 +19,10 @@ CREATE TABLE IF NOT EXISTS shops (
 """)
 conn.commit()
 
-# AUTH
-def get_shop(api_key: str):
-    print("INCOMING:", repr(api_key))
-
-    cursor.execute("SELECT * FROM shops")
-    rows = cursor.fetchall()
-    print("ALL ROWS:", rows)
-
-    cursor.execute(
-        "SELECT shop_id FROM shops WHERE api_key=?",
-        (api_key.strip(),)
-    )
-    result = cursor.fetchone()
-
-    print("MATCH:", result)
-
-    if not result:
-        raise HTTPException(status_code=401, detail="Invalid API Key")
-
-    return result[0]
-# ROOT
 @app.get("/")
 def root():
-    return {"status": "running"}
+    return {"status": "step 1 running"}
 
-# REGISTER
 @app.post("/register_shop")
 def register_shop():
     shop_id = str(uuid.uuid4())
@@ -62,40 +34,4 @@ def register_shop():
     return {
         "shop_id": shop_id,
         "api_key": api_key
-    }
-
-# PREDICT
-@app.post("/predict")
-def predict(data: dict, x_api_key: str = Header(..., alias="x-api-key")):
-
-    shop_id = get_shop(x_api_key)
-
-    features = np.array([[
-        data["order_hour"],
-        data["weekday"],
-        data["sku_complexity"],
-        data["warehouse_load"],
-        data["express"],
-        data["pick_time"],
-        data["shipping_time"]
-    ]])
-
-    prediction = model.predict(features)[0]
-    time = float(prediction)
-
-    return {
-        "shop": shop_id,
-        "prediction": {
-            "delivery_time_hours": round(time, 2),
-            "unit": "hours",
-            "risk_level": "high" if time > 25 else "normal"
-        }
-    }
-@app.get("/dashboard")
-def dashboard():
-    cursor.execute("SELECT COUNT(*) FROM shops")
-    shop_count = cursor.fetchone()[0]
-
-    return {
-        "total_shops": shop_count
     }
